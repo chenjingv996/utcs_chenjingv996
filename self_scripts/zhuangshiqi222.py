4c7d51e813c2d1d075aa16dcac39f99b00d8d2ee
chenjingv996 <349227607@qq.com>
2022-08-22 00:07:58 +0800

    update_0820

---
+++ self_scripts/zhuangshiqi222.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80)
+    
+def fn():
+    print("我是fn函数!")
+
+def add(a,b):
+    r=a+b
+    return r
+
+def mul(a,b,c):
+    r=a*b*c
+    return r
+
+def begin_end(old):
+    def new_fun(*args,**kwargs):
+        print("执行开始...")
+        res=old(*args,**kwargs)
+        return res
+        print("执行结束...")
+        #return res
+    return new_fun
+
+f3=begin_end(mul)
+r3=f3(2,3,4)
+
+print(r3)
+
