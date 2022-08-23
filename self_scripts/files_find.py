99396c7230629ffbb0489dd8f90785d3e98c1aa8
chenjingv <chenjingv>
2022-08-23 17:26:57 +0800

    update_0820

---
+++ self_scripts/files_find.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("*"*60)
+import glob
+
+lst1=glob.glob("*.py")
+for i in lst1:
+    print(i)
+print ("\n\n")
+
+lst2=glob.glob("*.sh")
+for j in lst2:
+    print(j)
+print ("\n\n")
+
+total=len(lst1)+len(lst2)
+
+print(f'该目录下共有{total}个文件，其中包含{len(lst1)}个python文件，包含{len(lst2)}个shell文件!')
+print ("\n\n")
