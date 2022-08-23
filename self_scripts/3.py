ebe3aa52146955946110b29c3d93e0d56c84d325
123 <123.qq.com>
2022-08-24 00:07:57 +0800

    update_0820

--- self_scripts/3.py
+++ self_scripts/3.py
@@ -7,13 +7,12 @@ while True:
         n = input()
         ta = []
         for i in range(int(n)):
-<<<<<<< HEAD
-            ta.append(int(random.randint(1,500)))
-=======
             ta.append(int(input()))
->>>>>>> f5d49cf25457c08b0f6a267f387120e6cb048f31
         uniq = set(ta)
         for j in sorted(uniq):
             print(j)
     except (EOFError, KeyboardInterrupt):
         break
+
+
+
