5940cbacc40b88d60fd95f58a1547eceb0ddc8c8
chenjingv <chenjingv>
2022-08-24 23:48:26 +0800

    update_0820

--- mianshi/1460_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-#coding:utf-8
-
-from collections import Counter
-class Solution:
-    def canBeEqual(self, target: list[int], arr: list[int]) -> bool:
-        return (Counter(target) == Counter(arr))
-
