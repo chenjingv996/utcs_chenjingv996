97e35acb71f3fce6deacaff592fe3b90a4d8d278
123 <123.qq.com>
2022-08-24 23:33:34 +0800

    update_0820

--- mianshi/1.py
+++ mianshi/1.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        N = len(nums)
+        for i in range(N):
+            rest = target - nums[i]
+            if rest in nums:
+                j = nums.index(rest) 
+                return [i,j]
+                break
+            else:
+                continue
 
+p1=Solution()
+r1=p1.twoSum([1,2,3,4],7)
+
+print(r1)
--- mianshi/1460.py
+++ mianshi/1460.py
@@ -5,12 +5,7 @@
 
 class Solution:
     def canBeEqual(self, target: list[int], arr: list[int]) -> bool:
-        a = sorted(target)
-        b = sorted(arr)
-        if a == b:
-            return True
-        else:
-            return False
+        return sorted(target) == sorted(arr)
 
 p1=Solution()
-p1.canBeEqual([1,2,3,4],[2,3,1,4])
+print(p1.canBeEqual([1,2,3,4],[2,3,1,4]))
