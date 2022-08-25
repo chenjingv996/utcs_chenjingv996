6a5fe43374f691c5585d7e4d0f074afc38cd600a
chenjingv <chenjingv>
2022-08-25 23:28:18 +0800

    update_0820

--- git_commit.py
+++ git_commit.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+
 from datetime import datetime
 import os
 import time 
+
 
 def git_commit():
     print("#"*50)
@@ -16,7 +18,8 @@ def git_commit():
     print(time.sleep(1))
     print("提交状态为:\n")
     print(os.system("git push\n"))
-    return git_commit
+
+
 if __name__=="__main__":
     git_commit()
     
