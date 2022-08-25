5a5959e11091f220b26c537466cce48d869c84a7
chenjingv <chenjingv>
2022-08-25 10:18:47 +0800

    update_0820

--- mianshi/1460.py
+++ mianshi/1460.py
@@ -15,3 +15,21 @@ r2=p1.canBeEqual([1,2,3,4],[3,4,1])
 print(r1)
 print(r2)
 print(Solution().canBeEqual([1,2,3,4],[3,4,1,2]))
+
+print("#"*66)
+
+class Chenjingv:
+    def fun1(self,target:int,nums:list[int])->list[int]:
+        N=len(nums)
+        for a in range(N):
+            res=target-nums[a]
+            if res in nums:
+                b=nums.index(res)
+                return [a,b]
+                break
+            else:
+                continue
+
+print(Chenjingv().fun1(5,[1,2,3,4]))
+
+
