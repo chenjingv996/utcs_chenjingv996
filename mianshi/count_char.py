b2566b7ee99728037016d8a299b02cdeedfbd34e
chenjingv <chenjingv>
2022-08-26 10:07:48 +0800

    update_0820

---
+++ mianshi/count_char.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+str1=input()
+str2=str1.lower()
+char1=input()
+char2=char1.lower()
+num=str2.count(char2)
+
+
+def count_char():
+    if 1<=len(str1)<=10:
+        if char2 in str2:
+            print(num)    
+        else:
+            print("不存在该字符!")
+    else:
+        print("字符串过长!")
+
+
+count_char()
+
+
