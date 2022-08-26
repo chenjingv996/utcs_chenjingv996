59ad2510db7fb6cfe12a0b61602a8e0daadd6c26
123 <123.qq.com>
2022-08-26 22:45:38 +0800

    update_0820

--- mianshi/longest_str.py
+++ mianshi/longest_str.py
@@ -2,7 +2,23 @@
 #coding:utf-8
 
 print("#"*80)
-    
-class ccc:
-    def aaa(self,s:str)->int:
 
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, -1
+        length = len(s)
+        res, freq = 0, []
+        while l < length:
+            if r + 1 < length and s[r + 1] not in freq:
+                r += 1
+                freq.append(s[r])
+            else:
+                freq.pop(0)
+                l += 1
+            res = max(res, r - l + 1)
+        return res
+
+
+print(Solution().lengthOfLongestSubstring(abcbaddfd))
