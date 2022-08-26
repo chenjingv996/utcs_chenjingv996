b4fa0a043ccc6cc515dc221f55b0769abab16b6e
chenjingv <chenjingv>
2022-08-27 00:47:21 +0800

    update_0820

--- mianshi/device_info.sh
+++ mianshi/device_info.sh
@@ -5,6 +5,7 @@
 from datetime import datetime
 import time
 import os
+import subprocess
 
 def device_info():
     print()
@@ -13,9 +14,13 @@ def device_info():
     print(f'当前时间为:{time.strftime("%Y-%m-%d %H:%M:%S")}\n')
     time.sleep(1)
     print(f'当前时间为:{time.ctime()}\n')
+    time.sleep(2)
+    t=subprocess.check_output("whoami")
+    print("name is:%s\n\n"%t)
     print("#"*80)
     print("\n\n")
-    print(os.system("uname -a"))
+    print(os.system("uname -r"))
+    print(os.system("arch"))
     print(os.system("ip add |grep brd"))
     print(os.system("pwd"))
     print(os.system("fdisk -l |grep sda"))
