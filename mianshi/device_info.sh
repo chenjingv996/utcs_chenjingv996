73571197624019c7b938259e3539c6451ccc0bc9
chenjingv <chenjingv>
2022-08-27 00:18:53 +0800

    update_0820

---
+++ mianshi/device_info.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+from datetime import datetime
+import time
+import os
+
+def device_info():
+    print()
+    print(f'当前时间为:{datetime.now().strftime("%Y_%m_%d %H:%M:%S")}\n')
+    time.sleep(1)
+    print(f'当前时间为:{time.strftime("%Y-%m-%d %H:%M:%S")}\n')
+    print("#"*80)
+    print("\n\n")
+    print(os.system("uname -a"))
+    print(os.system("ip add |grep brd"))
+    print(os.system("pwd"))
+    print(os.system("fdisk -l |grep sda"))
+   
+    
+
+
+if __name__=="__main__":
+    device_info()
