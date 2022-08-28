a503a53c4034159c13d70b1508634e643613c0cc
123 <123.qq.com>
2022-08-29 00:42:36 +0800

    update_0820

---
+++ mianshi/zhengshu_qufan.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+   
+
+class ccc:
+    def aaa(self, x: int) -> int:
+        str_num = str(x)
+        if str_num[0] == '-':
+            return -int(str_num[1:][::-1]) if int(str_num[1:][::-1]) < 2**31 else 0
+        else:
+            return int(str_num[::-1]) if int(str_num[::-1]) < 2**31-1 else 0
+
+
+
+print(ccc().aaa(-12345))
+print(ccc().aaa(-12340))
