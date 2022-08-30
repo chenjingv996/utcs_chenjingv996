0f57cf47a1a224506c4223428b48aac230d4dc4d
123 <123.qq.com>
2022-08-30 22:57:52 +0800

    update_0820

--- mianshi/pip_count.py
+++ mianshi/pip_count.py
@@ -4,15 +4,14 @@
 print("#"*80)
 
 from datetime import datetime
-from time
+import time
 import os
 import subprocess
 
 print(datetime.now().strftime("%Y-%m-%d %H:%M:%S")+"\n\n")
 
-curr_time=
-print(f'当前时间为:{time.ctime().strftime("%Y-%m-%d %H:%M:%S")})
+curr_time=time.ctime()
 
+print(f'当前时间为:{curr_time}\n\n')
 
-        curr_timetrftime("%Y-%m-%d %H:%M:%S")
 print(os.getcwd()+"\n\n")
