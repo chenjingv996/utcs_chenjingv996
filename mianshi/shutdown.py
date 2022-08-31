a12a1131f6304b1f1575ee7fdc738340f6ff58ef
123 <123.qq.com>
2022-08-31 22:09:56 +0800

    update_0820

--- mianshi/shutdown.py
+++ mianshi/shutdown.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-
-print("$"*80)
-
 import time
 from datetime import datetime
 import os
 import re
 
-n=int(input("是否需要关机(1.是 2.否):"))
+print("$"*80+"\n")
+print(time.ctime()+"\n")
+
+n=int(input("是否需要关机[1.是 2.否]:"))
 
 while True:
-    try:
-        if n==1:
-            os.system("shutdown -s -t 30")
-        elif n==2:
-            print("已取消关机操作！")
-            break
-        else:
-            print("输入错误！")
-            break
-    except:
+
+    if n==1:
+        os.system("shutdown -s -t 30")
+        break
+    elif n==2:
+        print("已取消关机操作！")
+        break
+    elif n>2 or n<1:
+        print("输入错误！")
+        break
+    else:
+        print("输入异常！")
         break
    
