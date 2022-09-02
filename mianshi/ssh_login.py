8530190065c43aad893d4826534814b0aa4af989
123 <123.qq.com>
2022-09-02 23:22:30 +0800

    update_0820

--- mianshi/ssh_login.py
+++ mianshi/ssh_login.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+
 import paramiko
+s = paramiko.SSHClient()
+s.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 允许连接不在know_hosts文件中的主机
+s.connect("192.168.0.123",22,"chenjingv", "123456")
+execmd = 'ls' #需要输入的命令
+stdin, stdout, stderr = s.exec_command (execmd)
+print(stdout.read())
+s.close()
 
-hostname='192.168.0.123'
-
-username='root'
-
-password='123456'
 
 
-if __name__=='__main__':
 
-    paramiko.util.log_to_file('paramiko.log')
-    
-    s=paramiko.SSHClient()
-    
-    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    
-    s.connect(hostname = hostname,username=username, password=password)
-    
-    stdin,stdout,stderr=s.exec_command('ifconfig;free;df -h')
-    
-    print(stdout.read())
-    
-    s.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
