bd06552b4f96d7e8ecdd5086ccde8af0b32aadaa
123 <123.qq.com>
2022-09-02 21:13:28 +0800

    update_0820

--- mianshi/ssh_login.py
+++ mianshi/ssh_login.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+import paramik
+
+hostname='192.168.0.123'
+
+username='root'
+
+password='123456'
+
+
+if __name__=='__main__':
+
+    paramiko.util.log_to_file('paramiko.log')
+    
+    s=paramiko.SSHClient()
+    
+    #s.load_system_host_keys()
+    
+    s.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    
+    s.connect(hostname = hostname,username=username, password=password)
+    
+    stdin,stdout,stderr=s.exec_command('ifconfig;free;df -h')
+    
+    print stdout.read()
+    
+    s.close()
 
