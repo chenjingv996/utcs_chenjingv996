e523da4a88087757101d9ba875720c1a0786e03b
123 <123.qq.com>
2022-09-03 11:55:00 +0800

    update_0820

--- mianshi/ssh_login.py
+++ mianshi/ssh_login.py
@@ -7,7 +7,7 @@ import paramiko
 s = paramiko.SSHClient()
 s.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # 允许连接不在know_hosts文件中的主机
 s.connect("192.168.0.123",22,"chenjingv", "123456")
-execmd = 'ip add'+'\n' #需要输入的命令
+execmd = ('pwd'+'\n')#需要输入的命令
 stdin, stdout, stderr = s.exec_command (execmd)
 print(stdout.read())
 s.close()
