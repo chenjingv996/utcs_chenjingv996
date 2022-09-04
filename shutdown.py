5905b63e9772331d1ae7fc97d2270be21b7830a6
123 <123.qq.com>
2022-09-04 23:48:36 +0800

    update_0820

---
+++ shutdown.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import time
+from datetime import datetime
+import os
+import re
+
+
+print("$"*80)
+res=input("是否需要关机(是[y/Y]/否[n/N]):")
+
+def get_result():
+    if res=="y" or res=="Y":
+        os.system("shutdown -s -t 30")
+    elif res=="n" or res=="N":
+        print("已取消关机操作！")
+    else:
+        print("输入错误！")
+
+
+if __name__=="__main__":
+    get_result()
