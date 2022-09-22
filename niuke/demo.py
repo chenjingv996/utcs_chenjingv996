230ec46fd5e5aecedfd931ccc875fa0555953a9b
chenjingv <chenjingv>
2022-09-22 10:06:56 +0800

    update_0820

--- niuke/demo.py
+++ niuke/demo.py
@@ -12,5 +12,6 @@ print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 str1=input("请输入一组字符串:")
-
 print(f'其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
+
+print(f'\n')
