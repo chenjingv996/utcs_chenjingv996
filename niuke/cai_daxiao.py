629cc6f304731fe5a593cba4ecc7ebbe2476fa5a
chenjingv <chenjingv>
2022-09-23 15:35:05 +0800

    update_0820

--- niuke/cai_daxiao.py
+++ niuke/cai_daxiao.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 #coding:utf-8
 
+import time
+from datetime import datetime
+import math
+import re
 import random
 
-num=random.randint(1,101)
-print(num)
-n=int(input())
+print("#"*80+"\n")
+print(f'当前时间为:{time.ctime()}\n')
+print(f'当前时间为:{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
-if n>num:
-    print("猜大了!")
-elif n<num:
-    print("猜小了!")
-else:
-    print("恭喜你，猜对了!")
+num=random.randint(1,101)
+print(str(num)+"\n")
+
+while True:
+    n=int(input("请输入一个数字:"))
+    if n>num:
+        print("猜大了!")
+    elif n<num:
+        print("猜小了!")
+    else:
+        print("恭喜你猜对了!")
+        break
+
+print()
