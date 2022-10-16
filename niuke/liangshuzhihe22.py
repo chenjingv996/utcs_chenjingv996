09766e11920188cff02ab9115a87b5a765bc1d54
123 <123.qq.com>
2022-10-16 11:25:12 +0800

    update_0820

--- niuke/liangshuzhihe22.py
+++ niuke/liangshuzhihe22.py
@@ -13,20 +13,25 @@ print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
-class ccc:
-    def aaa(self,nums,target):
-        d={}
-        n=len(nums)
-        for x in range(n):
-            if target-nums[x] in d:
-                return d[target-nums[x]],x
-            else:
-                d[nums[x]]=x
+# 时间复杂度：O(n^2)
+# 空间复杂度：O(1)
+def twoSum(self, nums: list[int], target: int) -> list[int]:
+    for i in range(len(nums)):
+        x = nums[i]
+        for j in range(i + 1, len(nums)):
+            if nums[j] == target - x:
+                return [i, j]
+    return []
 
-print(ccc().aaa([2,5,8,10],10))
-print(ccc().aaa([2,5,7,9,10],12))
-print(ccc().aaa([3,3],6))
-print(ccc().aaa([2],2))
-print(ccc().aaa([3],4))
+
+print(twoSum([1,2,3,4],5))
+
+
+
+#print(ccc().aaa([2,5,8,10],10))
+#print(ccc().aaa([2,5,7,9,10],12))
+#print(ccc().aaa([3,3],6))
+#print(ccc().aaa([2],2))
+#print(ccc().aaa([3],4))
 
 print(f'\n')
