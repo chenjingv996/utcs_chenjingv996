22941bf01679bfc82050687af9afb0122da67cd6
123 <123.qq.com>
2022-10-25 20:52:25 +0800

    update_0820

--- niuke/longest_str22.py
+++ niuke/longest_str22.py
@@ -9,6 +9,29 @@ import sys
 import re
 import math
 
+
+
+class ccc:
+    def aaa(self, s: str) -> int:
+
+        length = 0
+        
+        queue = []
+        for i in s:
+            if i in queue:
+                length = max(length, len(queue))
+                while i in queue:
+                    queue.pop(0)
+
+            queue.append(i)
+
+        return max(length, len(queue))
+
+
 print(ccc().aaa("abcdadaef"))
+print(ccc().aaa(""))
+print(ccc().aaa("ddd"))
+
+
 
 print(f'\n')
