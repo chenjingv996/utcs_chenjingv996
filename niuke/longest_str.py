b5e876ad19cb67a2792ac9bfde3a9eaca92d82a6
123 <123.qq.com>
2022-10-25 15:14:22 +0800

    update_0820

--- niuke/longest_str.py
+++ niuke/longest_str.py
@@ -9,12 +9,21 @@ import sys
 import re
 import math
 
-print(f"{time.ctime()}\n")
-print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+class ccc:
+    def aaa(self, s: str) -> int:
+        l, r = 0, -1
+        length = len(s)
+        res, freq = 0, []
+        while l < length:
+            if r + 1 < length and s[r + 1] not in freq:
+                r += 1
+                freq.append(s[r])
+            else:
+                freq.pop(0)
+                l += 1
+            res = max(res, r - l + 1)
+        return res
 
-
-
-str1=input("请输入一组字符串:")
-print(f'\n其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
+print(ccc().aaa("abcdadaef"))
 
 print(f'\n')
