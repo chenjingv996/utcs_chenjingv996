4cf64ad1ea174d406ce3c60cc1a781fb512091de
123 <123.qq.com>
2022-10-25 20:50:23 +0800

    update_0820

---
+++ niuke/longest_str22.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(ccc().aaa("abcdadaef"))
+
+print(f'\n')
