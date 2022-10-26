961997b724e4f71102e0689a9ed305a54cd76e44
chenjingv <chenjingv>
2022-10-26 16:00:45 +0800

    update_0820

---
+++ self_scripts/gougushuzu_bak.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+print("*"*60)
+import math
+ 
+max = 100  
+# 设定范围, a,b,c都在100以内
+pn = []  
+# Pythagorean Number 勾股数
+for a in range(2, int(max//math.sqrt(2))+1):
+    for b in range(a+1, int(math.sqrt(max*max - a*a))+1, 2):         
+        c = int(math.sqrt(s := a*a + b*b))
+        if c*c == s and math.gcd(a,b) == 1:
+            pn.append((a, b, c))
+
+for t in pn:
+    print(t)
+    print(math.sqrt(2))
+    print(math.sqrt(5))
+    print(int(max//math.sqrt(2)))
+
+print(f"Total = {len(pn)}")
+
