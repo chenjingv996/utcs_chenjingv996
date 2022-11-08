e5553b2c56f95c15ef38d340acb08943f5091e16
chenjingv <chenjingv>
2022-11-08 16:38:10 +0800

    update_0820

--- mysql_conn.sh
+++ mysql_conn.sh
@@ -7,15 +7,14 @@ host=192.168.3.123
 user=root
 pwd=123456
 
-mysql -u$user -p$pwd << EOF
-use mysql;
-select * from stus;
-EOF
 
 echo -e "${curr_time}\n"
 
-echo -e "$(date +%F_%T)\n"
 
-echo -e "`date +%F\ %H:%M:%S`\n"
+mysql -u$user -p$pwd << EOF
+use mysql;
+select * from stus where tel like "%138%";
+EOF
 
-echo -e "\n"
+
+echo -e "\n\n"
