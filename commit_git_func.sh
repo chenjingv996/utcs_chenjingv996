c27ed133f7ccde852d7649bc031d102b25696815
chenjingv <chenjingv>
2022-11-08 22:55:28 +0800

    update_0820

--- commit_git_func.sh
+++ commit_git_func.sh
@@ -3,7 +3,7 @@
 curr_time=`date +%F\ %T`
 
 commit_git(){
-    echo -e "`seq -s '#' 60|sed s/[0-9]//g`\n"
+    echo -e "\n`seq -s '#' 60|sed s/[0-9]//g`\n"
     echo -e "\n当前时间为:${curr_time}\n"
     echo -e "$(date +%F\ %T)\n"
     
