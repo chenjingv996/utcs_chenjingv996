8304a7379b8409e5a4beacea7af89eb3d31b7631
chenjingv <chenjingv>
2022-11-08 23:15:57 +0800

    update_0820

--- niuke/mysql_conn.sh
+++ niuke/mysql_conn.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-echo -e "######################################################\n"
 
 curr_time=`date +%F\ %H:%M:%S`
 host=192.168.3.123
 user=root
 pwd=123456
 
-mysql -u$user -p$pwd << EOF
-use mysql;
-select * from stus;
-EOF
-
+echo -e "`seq -s '#' 60|sed s/[0-9]//g`\n"
 echo -e "${curr_time}\n"
 
-echo -e "$(date +%F_%T)\n"
+mysql_conn()
+{
+mysql -u$user -p$pwd << EOF
+use mysql;
+select * from stus where tel like "%138%";
+select * from mysql.stus where addr="wuhan";
+EOF
 
-echo -e "`date +%F\ %H:%M:%S`\n"
+echo -e "\n\n"
 
-echo -e "\n"
+}
+
+
+mysql_conn
