a9211ec889ce2ac77d32af4daae88e71ba684056
chenjingv <chenjingv>
2022-11-08 22:52:46 +0800

    update_0820

--- mysql_conn.sh
+++ mysql_conn.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-echo -e "######################################################\n"
 
 curr_time=`date +%F\ %H:%M:%S`
 host=192.168.3.123
 user=root
 pwd=123456
 
-
+echo -e "`'*'*30`"
+echo -e "############################################################\n"
 echo -e "${curr_time}\n"
 
-
+mysql_conn()
+{
 mysql -u$user -p$pwd << EOF
 use mysql;
 select * from stus where tel like "%138%";
 EOF
 
-
 echo -e "\n\n"
+
+}
+
+
+mysql_conn
--- test.sh
+++ test.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-echo -e "######################################################\n"
 
 curr_time=`date +%F\ %H:%M:%S`
+
+echo -e "`seq -s '#' 60|sed s/[0-9]//g`\n"
 
 echo -e "${curr_time}\n"
 
 echo -e "$(date +%F_%T)\n"
 
 echo -e "`date +%F\ %H:%M:%S`\n"
+
+echo -e "`expr 3 \* 30`"
+
+echo -e "`expr 3 + 30`"
 
 echo -e "\n"
