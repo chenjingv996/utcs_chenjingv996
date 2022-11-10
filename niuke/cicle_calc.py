b38a1b266da476620f54d5a6f3bce7c75917df65
chenjingv <chenjingv>
2022-11-10 10:44:17 +0800

    update_0820

---
+++ niuke/cicle_calc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+class ccc:
+    def input_num():
+
+    def get_area(self,r:float)->float:
+        pi=3.1415
+        #r=float(input("请输入园的边长:"))
+        area=pi*r*r
+        return area
+
+
+print(ccc().get_area(123))
+    
+
