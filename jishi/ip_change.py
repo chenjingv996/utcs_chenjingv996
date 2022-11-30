eadbfddaa1d6f0bc718ff5c9068a38a2dcd69bd7
123 <123.qq.com>
2022-11-30 16:45:10 +0800

    update_0820

--- jishi/ip_change.py
+++ jishi/ip_change.py
@@ -14,7 +14,12 @@ print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
 
-str1=input("请输入一组字符串:")
-print(f'\n其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
+while True:
+    try:
+        print(sum([256 ** j * int(i) for j, i in enumerate(input().split('.')[::-1])]))
+        b = int(input())
+        print('.'.join([str(b // (256 ** i) % 256) for i in range(3, -1, -1)]))
+    except:
+        break
 
 print(f'\n')
