3813e1fae9437d9dd6a7ed55531ec1502cdfcf03
chenjingv <chenjingv>
2022-11-30 15:28:33 +0800

    update_0820

---
+++ jishi/count_char22.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+str=input()
+print(len(set(str)))
+
+print(f'\n')
