5020ec5911ef5136d32c71eee50bfe76741f3315
chenjingv <chenjingv>
2022-11-30 14:48:02 +0800

    update_0820

---
+++ jishi/jinzhi_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+
+while True:
+    try:
+        print(int(input("请输入一个16进制数:"),16))
+    except:
+        break
+
+
+print(f'\n')
