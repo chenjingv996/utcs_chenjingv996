70162170487c6f7284ae1de8f3c12a7ab8820db4
123 <123.qq.com>
2022-11-30 16:30:54 +0800

    update_0820

--- jishi/zuobiao.py
+++ jishi/zuobiao.py
@@ -13,8 +13,20 @@ print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
+str = input().split(';')
+x, y = 0, 0
+for i in str:
+    if 2 <= len(i) <= 3:
+        if i[0] in ['A', 'D', 'W', 'S'] and i[1:].isdigit():
+            if i[0] == 'A':
+                x -= int(i[1:])
+            if i[0] == 'D':
+                x += int(i[1:])
+            if i[0] == 'W':
+                y += int(i[1:])
+            if i[0] == 'S':
+                y -= int(i[1:])
+print('{0},{1}'.format(x, y))
 
-str1=input("请输入一组字符串:")
-print(f'\n其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
 
 print(f'\n')
