621c1c72776bd79585db3a1ee563eeb9fdd97c2f
chenjingv <chenjingv>
2022-12-02 02:32:56 +0800

    update_0820

---
+++ jishi/longest_huiwen.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+
+while True:
+    try:
+        s=input()
+        res=[]
+        for i in range(len(s)):
+            for j in range(i+1,len(s)+1):
+                if s[i:j]==s[i:j][::-1]:
+                    res.append(j-1)
+        if res != '':
+            print(max(res))
+    except:
+        break
+
+
+
+print(f'\n')
