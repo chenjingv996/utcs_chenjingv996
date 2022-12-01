b7e718c4ff2f5695243fd30640b8b412d20aa16a
123 <123.qq.com>
2022-12-02 02:52:33 +0800

    update_0820

--- jishi/longest_huiwen22.py
+++ jishi/longest_huiwen22.py
@@ -22,11 +22,24 @@ while True:
                 if s[i:j]==s[i:j][::-1]:
                     res.append(j-1)
         if res != '':
-            print(res)
             print(max(res))
     except:
         break
 
+
+while True:
+    try:
+        s = input()
+        res = []
+        
+        for i in range(len(s)):
+            for j in range(i+1, len(s)+1):
+                if s[i:j] == s[i:j][::-1]:
+                    res.append(j-i)
+        if res != '':
+            print(max(res))
+    except:
+        break
 
 
 print(f'\n')
