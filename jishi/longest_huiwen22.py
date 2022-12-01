7d20ff9643d2f779760c68773b75d4d3d15e6d97
chenjingv <chenjingv>
2022-12-02 02:58:12 +0800

    update_0820

--- jishi/longest_huiwen22.py
+++ jishi/longest_huiwen22.py
@@ -13,18 +13,18 @@ print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
-while True:
-    try:
-        s=input()
-        res=[]
-        for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                if s[i:j]==s[i:j][::-1]:
-                    res.append(j-1)
-        if res != '':
-            print(max(res))
-    except:
-        break
+#while True:
+#    try:
+#        s=input()
+#        res=[]
+#        for i in range(len(s)):
+#            for j in range(i+1,len(s)+1):
+#                if s[i:j]==s[i:j][::-1]:
+#                    res.append(j-1)
+#        if res != '':
+#            print(max(res))
+#    except:
+#        break
 
 
 while True:
@@ -37,6 +37,7 @@ while True:
                 if s[i:j] == s[i:j][::-1]:
                     res.append(j-i)
         if res != '':
+            print(res)
             print(max(res))
     except:
         break
