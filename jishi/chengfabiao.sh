5cdb81d8278f30b47eed767d9dab11066c7b5073
123 <123.qq.com>
2022-12-06 22:33:22 +0800

    update_0820

--- jishi/chengfabiao.sh
+++ jishi/chengfabiao.sh
@@ -13,6 +13,18 @@ print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
+#!/bin/bash
+ 
+for i in `seq  9`
+do
+    for j in `seq  $i`       #这地方写成$i 就比写成seq 9 方便多了呢
+    do
+        echo  -n  "$i*$j=$(($i*$j)) "    #有一个空格
+    done
+    echo             内层循环完成以后换行
+done
+echo
+
 
 
 print(f'\n')
