7450fb9586e3a2ec36653bc683521fe5510de23c
chenjingv <chenjingv>
2022-12-07 00:04:16 +0800

    update_0820

--- jishi/chengfabiao.sh
+++ jishi/chengfabiao.sh
@@ -8,7 +8,7 @@ for i in `seq  9`
 do
     for j in `seq  $i`       #这地方写成$i 就比写成seq 9 方便多了呢
     do
-        echo  -n  "$i*$j=$(($i*$j)) "    #有一个空格
+        echo  -n  "$j*$i=$(($i*$j))  "    #有一个空格
     done
     echo             #内层循环完成以后换行
 done
