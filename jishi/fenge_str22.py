d732f3cbe9dbedd48bbfc938fefb26c47cb93b6b
chenjingv <chenjingv>
2022-12-12 11:35:00 +0800

    update_0820

--- jishi/fenge_str22.py
+++ jishi/fenge_str22.py
@@ -41,10 +41,6 @@ for s in lf:
     l.append(check(s))
 print("-".join(l))
 
-#作者：无限
-#链接：https://leetcode.cn/circle/discuss/niKSMZ/view/mRMgo3/
-#来源：力扣（LeetCode）
-#著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 print(f'\n')
