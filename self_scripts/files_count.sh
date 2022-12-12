bb3f7f509bb9015e25140ae85092420fa36dbd51
chenjingv <chenjingv>
2022-12-12 19:44:38 +0800

    update_0820

--- self_scripts/files_count.sh
+++ self_scripts/files_count.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-self_path="/home/chenjing/utcs_chenjingv996/self_scripts"
+self_path=`pwd`
 
 py_info=`ls -l *.py`
 sh_info=`ls -l *.sh`
+py_count=`ls -l *.py|wc -l`
+sh_count=`ls -l *.sh|wc -l`
 
 fn_start()
 {
-    echo -e "\n"
-    echo -e "########################计算开始####################################"
+    echo -e 
+    echo -e "`seq -s '#' 30|sed s/[0-9]//g`测试执行开始`seq -s '#' 30|sed s/[0-9]//g`\n"
+    echo -e 
 }
+
 
 fn_stop()
 {
-    echo -e "\n"
-    echo -e "########################计算结束####################################"
+    echo -e 
+    echo -e "`seq -s '#' 30|sed s/[0-9]//g`测试执行结束`seq -s '#' 30|sed s/[0-9]//g`\n"
+    echo -e 
 }
 
 scripts_total()
@@ -23,15 +28,16 @@ scripts_total()
     cd ${self_path}	
     echo -e "python脚本为:\n$py_info\n"
     echo -e "shell脚本为:\n$sh_info\n"
-    echo -e "该文件夹下包括$(ls -l *.py|wc -l)个python脚本，$(ls -l *.sh|wc -l)个shell脚本!"
+    echo -e "当前目录为:$self_path,该目录下包括$py_count个python脚本，$sh_count个shell脚本!"
     fn_stop
 }
 
 main_menu()
 {
+    echo -e	
     echo -e "当前时间为:"$(date "+%Y-%m-%d %H:%M:%S")
     scripts_total
-    echo -e "\n\n"
+    echo -e 
 }
 
 
