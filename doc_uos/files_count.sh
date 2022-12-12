fda716767c3f128e5e5947dbd332bb7f15e5170f
chenjingv <chenjingv>
2022-12-12 20:41:18 +0800

    update_0820

--- doc_uos/files_count.sh
+++ doc_uos/files_count.sh
@@ -6,6 +6,7 @@ py_info=`ls -l *.py`
 sh_info=`ls -l *.sh`
 py_count=`ls -l *.py|wc -l`
 sh_count=`ls -l *.sh|wc -l`
+total_count=`ls -l *.py *.sh|wc -l`
 
 fn_start()
 {
@@ -28,7 +29,7 @@ scripts_total()
     cd ${self_path}	
     echo -e "python脚本为:\n$py_info\n"
     echo -e "shell脚本为:\n$sh_info\n"
-    echo -e "当前目录为:$self_path,该目录下包括$py_count个python脚本，$sh_count个shell脚本!"
+    echo -e "当前目录为:$self_path,该目录下共有$total_count个脚本文件,\n其中包含$py_count个python脚本，$sh_count个shell脚本!"
     fn_stop
 }
 
