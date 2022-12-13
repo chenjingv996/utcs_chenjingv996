fca03e2ace6be71350a9edbfa00cbba5b41ec5c4
chenjingv <chenjingv>
2022-12-13 18:00:59 +0800

    update_0820

--- mianshi/commit_git_func.sh
+++ mianshi/commit_git_func.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curr_time=`date +%F\ %T`
-git_dir="/home/chenjingv/utcs_chenjingv996"
+curr_dir=`pwd`
 
 commit_git(){
     echo -e "\n`seq -s '#' 60|sed s/[0-9]//g`\n"
@@ -9,12 +9,12 @@ commit_git(){
     echo -e "$(date +%F\ %T)\n"
     
     git status
-    echo -e "\n"
+    echo -e 
     
     cat ~/.gitconfig
-    echo -e "\n"
+    echo -e "\n当前目录为:$curr_dir\n"
     
-    git add $git_dir && git commit -m "update_0820" && git push
+    git add $curr_dir && git commit -m "update_0820" && git push
     echo -e "\n\ngit提交后状态为:\n"
     
     git status
