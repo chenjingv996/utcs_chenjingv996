7285b68f103e428daf3617d9cabfe1a39368f994
chenjingv <chenjingv>
2022-12-13 18:12:33 +0800

    update_0820

--- commit_git_func.sh
+++ commit_git_func.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curr_time=`date +%F\ %T`
+curr_dir=`pwd`
 
 commit_git(){
     echo -e "\n`seq -s '#' 60|sed s/[0-9]//g`\n"
@@ -8,12 +9,12 @@ commit_git(){
     echo -e "$(date +%F\ %T)\n"
     
     git status
-    echo -e "\n"
+    echo -e 
     
     cat ~/.gitconfig
-    echo -e "\n"
+    echo -e "\n当前目录为:$curr_dir\n"
     
-    git add . && git commit -m "update_0820" && git push
+    git add $curr_dir && git commit -m "update_0820" && git push
     echo -e "\n\ngit提交后状态为:\n"
     
     git status
--- demo.py
+++ demo.py
@@ -15,6 +15,11 @@ print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
 str1=input("请输入一组字符串:")
+
 print(f'\n其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
+
+print(f'\n{list(map(lambda x:x**2,range(11)))}')
+
+print(f'\n{list(map(str,range(5)))}')
 
 print(f'\n')
