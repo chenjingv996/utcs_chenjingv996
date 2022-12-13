75590aa3c3ce23502ac852b5bc275a1460203b05
chenjingv <chenjingv>
2022-12-13 23:30:18 +0800

    update_0820

--- jishi/chengfabiao22.py
+++ jishi/chengfabiao22.py
@@ -10,7 +10,8 @@ print(f'{tm.ctime()}\n')
 
 for i in range(1, 10):
     for j in range(1, i+1):
-        print('{}x{}={}\t'.format(j, i, i*j), end='')
+        print('%d*%d=%-3d'%(j, i, i*j), end='\t')
     print()
+print()
 
 
