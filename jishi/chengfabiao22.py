093a638929074e638bc1e1d1cfb9bcf61d2566d2
chenjingv <chenjingv>
2022-12-13 23:22:25 +0800

    update_0820

---
+++ jishi/chengfabiao22.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print(f"{'#'*60}\n")
+
+from datetime import datetime as dt
+import time as tm
+
+print(f'{tm.ctime()}\n')
+
+for i in range(1, 10):
+    for j in range(1, i+1):
+        print('{}x{}={}\t'.format(j, i, i*j), end='')
+    print()
+
+
