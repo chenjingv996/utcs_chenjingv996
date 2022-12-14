99fa58c06b3abe0edaf284ead84d7d90b687f894
123 <123.qq.com>
2022-12-14 20:45:43 +0800

    update_0820

--- jishi/duicheng_mimajiequ.py
+++ jishi/duicheng_mimajiequ.py
@@ -15,18 +15,21 @@ print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 print(f'{sys.version}\n')
 
 
-aa=sys.stdin.readline()
-bb=input("请输入bb:")
-print(len(aa))
-print(len(bb))
+password = input()
+max_length = 1
+for i in range(1, len(password)):
+    # 直接以已知最大长度开始
+    for j in range((max_length-1) // 2 , len(password[0:i])):
+        # 以当前下标字符 的 下一个字符为对称轴，判断两边是否一样
+        if password[i - j - 1:i + 1] == password[i + j + 1:i - 1:-1]:
+            max_length = len(password[i - j - 1:i + 1]) * 2 - 1
+        # 以当前下标字符 和 下一个字符为对称轴
+        elif password[i - j - 1:i] == password[i + j:i - 1:-1]:
+            max_length = len(password[i - j - 1:i]) * 2
+        else:
+            # 从最长字符串的长度逐渐递增,不相等则直接跳出
+            break
+print(max_length)
 
-
-str1=input("请输入一组字符串:")
-
-print(f'\n其中最后一个字符串长度为:{len(str1.split(" ")[-1])}')
-
-print(f'\n{list(map(lambda x:x**2,range(11)))}')
-
-print(f'\n{list(map(str,range(5)))}')
 
 print(f'\n')
