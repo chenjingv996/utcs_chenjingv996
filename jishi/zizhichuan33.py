747c8eb4e1fc57039f00ab35c4e487456b448d21
chenjingv <chenjingv>
2022-12-14 22:56:24 +0800

    update_0820

---
+++ jishi/zizhichuan33.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime as dt
+import sys
+import re
+import math
+import let 
+
+print(f"{time.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+str1 = input()
+str2 = input()
+tmp = ''
+
+for i in range(len(str1)):
+    for j in range(len(str2)):
+        if str1[i] == str2[j]:
+            tmp += str1[i]
+            str2=str2[j+1:]
+            break
+
+if tmp==str1:
+    print("true") 
+else:
+    print('false')
+
+
+print(f'\n')
+
+
