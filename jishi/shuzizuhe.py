2ce763a7bdca639d2eabbc702ba49243d5abcd22
chenjingv <chenjingv>
2022-12-14 23:41:46 +0800

    update_0820

---
+++ jishi/shuzizuhe.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime as dt
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+num1=int(input())
+
+
+
+print(f'\n')
