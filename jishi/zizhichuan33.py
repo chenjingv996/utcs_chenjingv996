bc669fdacc97363d499119d9c4c89af04bc9b434
chenjingv <chenjingv>
2022-12-14 22:59:08 +0800

    update_0820

--- jishi/zizhichuan33.py
+++ jishi/zizhichuan33.py
@@ -22,7 +22,7 @@ for i in range(len(str1)):
         if str1[i] == str2[j]:
             tmp += str1[i]
             str2=str2[j+1:]
-            print(str2)
+            print(str2)       #debug
             break
 
 if tmp==str1:
