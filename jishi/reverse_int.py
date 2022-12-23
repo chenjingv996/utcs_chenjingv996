cd364dcc9ad7bbe5834c76ef8864282168723606
chenjingv <chenjingv>
2022-12-23 08:00:26 +0800

    update_0820

---
+++ jishi/reverse_int.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime as dt
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+print(f'{sys.version}\n')
+
+
+aa=sys.stdin.readline()
+bb=input("请输入bb:")
+print(len(aa))
+print(len(bb))
+
+
+def res(self,x:int)->int:
+
+print(f'\n')
