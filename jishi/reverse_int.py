d421e9522337b02c737962561c6068d17f52711a
chenjingv <chenjingv>
2022-12-23 08:25:42 +0800

    update_0820

--- jishi/reverse_int.py
+++ jishi/reverse_int.py
@@ -12,15 +12,21 @@ import math
 print(f"{time.ctime()}\n")
 print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
-print(f'{sys.version}\n')
+class ccc:
+   def aaa(self,x:int)->int:
+       if x==0:
+           return 0
+       elif x>0:
+           return int(str(x)[::-1])
+       else:
+           return -int(str(abs(x))[::-1])
+
+bbb=ccc()
 
 
-aa=sys.stdin.readline()
-bb=input("请输入bb:")
-print(len(aa))
-print(len(bb))
-
-
-def res(self,x:int)->int:
+print(bbb.aaa(0))
+print(bbb.aaa(-123))
+#print(bbb.aaa("qwew"))
+print(bbb.aaa(234))
 
 print(f'\n')
