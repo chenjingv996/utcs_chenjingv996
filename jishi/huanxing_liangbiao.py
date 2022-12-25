d961b623a65bf2910547e4de89359b87add69459
123 <123@qq.com>
2022-12-25 22:43:18 +0800

    update_0820

---
+++ jishi/huanxing_liangbiao.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime as dt
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+
+class ccc:
+    def aaa(self, head: list) -> bool:
+        # 1. python map
+        m = {}
+        while head:
+            if m.get(head):
+                return True
+            m[head] = 1
+            head = head.next
+        return False
+
+bbb=ccc()
+
+print(bbb.aaa([3,2,0,-4],1))
+
+
+
+print(f'\n')
