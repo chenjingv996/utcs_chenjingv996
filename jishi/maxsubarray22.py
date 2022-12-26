54bc050605b9c2cf0ac58c88f0665fe9defe275c
123 <123@qq.com>
2022-12-26 10:04:56 +0800

    update_0820

---
+++ jishi/maxsubarray22.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+
+
+class ccc:
+    def aaa(self, nums: list[int]) -> int:
+        tmp = nums[0]
+        max_ = tmp
+        n = len(nums)
+        for i in range(1,n):
+            # 当当前序列加上此时的元素的值大于tmp的值，说明最大序列和可能出现在后续序列中，记录此时的最大值
+            if tmp + nums[i]>nums[i]:
+                max_ = max(max_, tmp+nums[i])
+                tmp = tmp + nums[i]
+            else:
+            #当tmp(当前和)小于下一个元素时，当前最长序列到此为止。以该元素为起点继续找最大子序列,
+            # 并记录此时的最大值
+                max_ = max(max_, tmp, tmp+nums[i], nums[i])
+                tmp = nums[i]
+        return max_
+        
+
+
+
+
+bbb=ccc()
+print(bbb.aaa([-2,1,-3,4,-1,2,1,-5,4]))
+
+print(f'\n')
