68e30a9f8253c092c986286437419227e0f30ae9
123 <123@qq.com>
2022-12-26 18:21:12 +0800

    update_0820

---
+++ jishi/erfen_find22.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import time as tm
+from datetime import datetime as dt
+
+class ccc:
+    def aaa(self,nums:list[int],target:int)->int:
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                return mid
+            elif target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return -1
+
+bbb=ccc()
+print(bbb.aaa([1,2,3,4,5],6))
