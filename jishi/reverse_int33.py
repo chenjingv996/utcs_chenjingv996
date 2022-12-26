5571b17fb0c91ac8b7545a0f484a2491ac37985c
chenjingv <chenjingv>
2022-12-26 22:13:39 +0800

    update_0820

---
+++ jishi/reverse_int33.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime as dt
+import sys
+import re
+import math
+
+print(f"{time.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+class ccc:
+    def aaa(self,x:int)->int:
+        if x==0:
+            return 0
+        elif x>0:
+            return int(str(x)[::-1])
+        else:
+            return -int(str(-x)[::-1])
+
+bbb=ccc()
+print(bbb.aaa(123))
+print(bbb.aaa(-234))
+print(bbb.aaa(0))
+
+
+print(f'\n')
