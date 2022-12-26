c849395a6acf1c24c31aeb963646cb9b2657d692
123 <123@qq.com>
2022-12-26 21:37:26 +0800

    update_0820

--- jishi/erfen_find22.py
+++ jishi/erfen_find22.py
@@ -9,6 +9,7 @@ print("\n"+"#"*80+"\n")
 print(f"\n当前时间为:{tm.ctime()}\n")
 print(f"\n当前时间为:{dt.now()}\n")
 
+
 class ccc:
     def aaa(self,nums:list[int],target:int)->int:
         l,r=0,len(nums)-1
