2d6689e743348cdbd642992e5b9f1509fea7df57
123 <123@qq.com>
2022-12-27 10:34:05 +0800

    update_0820

---
+++ jishi/reverse_int44.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time as tm
+from datetime import datetime as dt
+import sys
+import re
+import math
+
+print(f"{tm.ctime()}\n")
+print(f'{dt.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+class ccc:
+    def aaa(self,x:int)->int:
+        if x==0:
+            return 0
+        elif x>0:
+            return "".join(reversed(str(x)))
+        else:
+            return str(x)[0]+"".join(reversed(str(-x)))
+
+bbb=ccc()
+print(bbb.aaa(123))
+print(bbb.aaa(-234))
+print(bbb.aaa(0))
+
+
+print(f'\n')
