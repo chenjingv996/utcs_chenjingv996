35885bad1143c0ad85e0622a0b884dc096bda87b
chenjingv <chenjingv>
2022-12-29 23:45:05 +0800

    update_0820

--- jishi/threesum.py
+++ jishi/threesum.py
@@ -12,5 +12,26 @@ import math
 print(f"{time.ctime()}\n")
 print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
+class ccc:
+    def aaa(self,nums:list[int])->list[int]:
+        n,ans=len(nums),[]
+        nums.sort()
+        for i,e in enumerate(nums):
+            if i and e == nums[i-1]:
+                continue
+            l,r,target=i+1,n-1,-e
+            while l<r:
+                cur=nums[l]+nums[r]
+                if cur==target:
+                    if not ans or [e,nums[l],nums[r]]!=ans[-1]:
+                        ans.append([e,nums[l],nums[r]])
+                    l+=1
+                    r-=1
+                elif cur<target:l+=1
+                else:r-=1
+        return ans
+
+bbb=ccc()
+print(bbb.aaa([-1,0,1,2,-1,4]))
 
 print(f'\n')
