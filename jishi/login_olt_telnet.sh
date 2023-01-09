b33f26ed8a50eab553b5c931bb5d129ee9c1fd94
chenjingv <chenjingv>
2023-01-09 21:00:15 +0800

    update_0820

---
+++ jishi/login_olt_telnet.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/expect -f
+
+set host_ip [lindex $argv 0]
+set ad "chenjingv"
+set pw "123456"
+set timeout 60
+
+spawn telnet $host_ip
+
+expect "*gin:"
+send "$ad\r"
+expect "*ord:"
+send "$pw\r"
+sleep 1
+expect "*:*"
+send "su\r"
+expect "*：*"
+send "$pw\r"
+sleep 1
+expect "*#"
+send "ip add\r"
+expect "*#"
+send "\r\r"
+
+interact
+
