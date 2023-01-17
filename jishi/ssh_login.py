1aa510dee397e7a8e57c4e35a60cc3b68069dcba
chenjingv <chenjingv>
2023-01-17 23:31:21 +0800

    update_0820

--- jishi/ssh_login.py
+++ jishi/ssh_login.py
@@ -8,7 +8,7 @@ client = paramiko.SSHClient()
 client.load_system_host_keys()
  
 # connect to client
-client.connect('192.168.3.123',22,'chenjingv','123456',allow_agent=False,look_for_keys=False)
+client.connect("192.168.3.123",22,"chenjingv","123456",allow_agent=False,look_for_keys=False)
  
 # get shell
 ssh_shell = client.invoke_shell()
@@ -16,17 +16,17 @@ ssh_shell = client.invoke_shell()
 while True:
     line = ssh_shell.recv(1024)
     #print line
-    if line and line.endswith(b'\$'):
+    if line and line.endswith(b"$"):
         break;
  
 # send command
-ssh_shell.sendall( 'ping 192.168.3.123' + '\n')
+ssh_shell.sendall("ping 192.168.3.123" + "\n")
  
 # get result lines
 lines = []
 while True:
     line = ssh_shell.recv(1024)
-    if line and line.endswith(b'\$'):
+    if line and line.endswith(b"\$"):
         break;
     lines.append(line)
 result = ''.join(lines)
