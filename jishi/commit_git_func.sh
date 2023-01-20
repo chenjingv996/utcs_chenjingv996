1eeb994b524de42999975c960b86928fb8ec5264
chenjingv <chenjingv>
2023-01-20 10:45:18 +0800

    update_0820

--- jishi/commit_git_func.sh
+++ jishi/commit_git_func.sh
@@ -7,7 +7,9 @@ commit_git(){
     echo -e "\n`seq -s '#' 60|sed s/[0-9]//g`\n"
     echo -e "\n当前时间为:${curr_time}\n"
     echo -e "$(date +%F\ %T)\n"
-    
+   
+    chmod a+x *.py *.sh
+
     git status
     echo -e 
     
