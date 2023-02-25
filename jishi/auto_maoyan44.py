df5ea74b7e0b7e018f1012116813078c4ec60b95
chenjingv <chenjingv>
2023-02-25 18:49:20 +0800

    update_0820

--- jishi/auto_maoyan44.py
+++ jishi/auto_maoyan44.py
@@ -5,8 +5,15 @@
 #paramiko是ansible重要模板之一，支持SSH2远程安全连接，支持认证及密钥方式。可以实现远程命
 #令执行、文件传输、中间SSH代理等功能
 import paramiko
-cmd = "pwd"
+import time
+from datetime import datetime
+import sys
+
+cmd = "pwd && arch && who"
 task_info = "ps -aux"
+
+
+
 # 创建客户端对象
 ssh = paramiko.SSHClient()
 # 接收并保存新的主机名，此外还有RejectPolicy()拒绝未知的主机名
@@ -16,6 +23,23 @@ ssh.connect(hostname="192.168.3.123", username="chenjingv", password="123456",
 port=22)
 # 执行命令，timeout为此次会话的超时时间，返回的是(stdin, stdout, stderr)的三元组
 stdin, stdout, stderr = ssh.exec_command(cmd, timeout=20)
+
+while not stdout.channel.exit_status_ready():
+    result = stdout.readline()
+    print(result)
+    if stdout.channel.exit_status_ready():
+        result = stdout.readlines()
+        print(result)
+        break
+
 # 需要解码才能把返回的内容转换为正常的字符串形式
-#print(stdin.read().decode())
-print(stdout.read().decode())
+#in_data=stdin.write(cmd)
+
+
+#res=out_data if out_data else err_data
+
+#print(f'\n输入命令为:{cmd}\n')
+#print(f'\n输出结果为:{result}\n')
+
+ssh.close()
+        
