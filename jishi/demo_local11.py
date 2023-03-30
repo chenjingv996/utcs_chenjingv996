7221d419046bdf4bd3a24db28901c1d319c15b53
chenjingv <chenjingv>
2023-03-30 20:35:51 +0800

    update_0820

--- jishi/demo_local11.py
+++ jishi/demo_local11.py
@@ -11,33 +11,38 @@ import time
 import os,sys
 
 
-log_file=open(os.path.join(os.getcwd(),'run_temp.log'),'w')
-sys.stdout=log_file
+output=open(os.path.join(os.getcwd(),'run_temp.log'),'w')
+#sys.stdout=log_file
 
-class TelnetClient():
-    def __init__(self,):
+class TelnetClient:
+    def __init__(self):
+        self.host_ip='192.168.3.123'
+        self.username='chenjingv'
+        self.password='123456'
+        self.cmd_1='su'
         self.tn = telnetlib.Telnet()
 
+
     # 此函数实现telnet登录主机
-    def login_host(self,host_ip,username,password,cmd_1='su'):
+    def login_host(self):
         try:
             # self.tn = telnetlib.Telnet(host_ip,port=23)
-            self.tn.open(host_ip,port=23)
+            self.tn.open(self.host_ip,port=23)
         except:
-            logging.warning('%s网络连接失败'%host_ip)
+            logging.warning(f'{self.host_ip}网络连接失败!\n')
             return False
         # 等待login出现后输入用户名，最多等待10秒
         self.tn.read_until(b'gin: ',timeout=10)
-        self.tn.write(username.encode('ascii') + b'\n')
+        self.tn.write(self.username.encode('ascii') + b'\n')
         # 等待Password出现后输入用户名，最多等待10秒
         self.tn.read_until(b'word: ',timeout=10)
-        self.tn.write(password.encode('ascii') + b'\n')
+        self.tn.write(self.password.encode('ascii') + b'\n')
 
         self.tn.read_until(b'$ ',timeout=10)
-        self.tn.write(cmd_1.encode('ascii') + b'\n')
+        self.tn.write(self.cmd_1.encode('ascii') + b'\n')
 
         self.tn.read_until(b' ',timeout=10)
-        self.tn.write(password.encode('ascii') + b'\n')
+        self.tn.write(self.password.encode('ascii') + b'\n')
         
         #self.tn.read_until(b'# ',timeout=10)
         #self.tn.write(cmd_2.encode('ascii') + b'\n')
@@ -47,33 +52,37 @@ class TelnetClient():
         # read_very_eager()获取到的是的是上次获取之后本次获取之前的所有输出
         command_result = self.tn.read_very_eager().decode('ascii')
         if 'Login incorrect' not in command_result:
-            logging.warning('%s登录成功'%host_ip)
+            logging.warning(f'{self.host_ip}登录成功!\n')
             return True
         else:
-            logging.warning('%s登录失败，用户名或密码错误'%host_ip)
+            logging.warning(f'{self.host_ip}登录失败，用户名或密码错误!\n')
             return False
 
     # 此函数实现执行传过来的命令，并输出其执行结果
     def execute_some_command(self,command):
+        self.login_host()
         # 执行命令
         self.tn.write(command.encode('ascii')+b'\n')
         time.sleep(2)
         # 获取命令结果
         command_result = self.tn.read_very_eager().decode('ascii')
-        logging.warning('命令执行结果：\n%s' % command_result)
+        res=str(command_result)
+        self.tn.logfile=output.write(f'{res}\n')
+        logging.warning(f'命令执行结果：\n{command_result}')
+        self.logout_host()    
 
     # 退出telnet
     def logout_host(self):
         self.tn.write(b"exit\n")
 
 if __name__ == '__main__':
-    host_ip = '192.168.3.123'
-    username = 'chenjingv'
-    password = '123456'
+    #host_ip = '192.168.3.123'
+    #username = 'chenjingv'
+    #password = '123456'
     command = 'arch'
     telnet_client = TelnetClient()
     # 如果登录结果返加True，则执行命令，然后退出
-    if telnet_client.login_host(host_ip,username,password):
-        telnet_client.execute_some_command(command)
-        telnet_client.logout_host()
+    #if telnet_client.login_host():
+    telnet_client.execute_some_command(command)
+    #    telnet_client.logout_host()
 
