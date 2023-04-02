4ec6870229b598477dfe9d9e35fb6ba06f2f45f0
chenjingv <chenjingv>
2023-04-02 10:02:36 +0800

    update_0820

--- jishi/demo_local33.py
+++ jishi/demo_local33.py
@@ -93,7 +93,10 @@ class TelnetClient:
     @zhuangsiqi
     def check_ssh(self):
         self.login_host()
-        cmds=['ip add | grep inet -C2','ps -ef | grep sshd','netstat -anp | grep :22']
+        cmds=['ip add | grep inet -C2',
+              'ps -ef | grep sshd',
+              'netstat -anp | grep :22',
+              'chenjingv good!']
         for i in range(len(cmds)):
             self.tn.write(cmds[i].encode('ascii')+b'\n')
             time.sleep(1)
