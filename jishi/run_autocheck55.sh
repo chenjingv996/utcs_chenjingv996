912d81f72a5a8d2d17a022760a0dcac907cb6085
chenjingv <chenjingv>
2023-04-02 12:16:06 +0800

    update_0820

--- jishi/run_autocheck55.sh
+++ jishi/run_autocheck55.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-rm run_local_console.log &&  ./demo_local55_logfile.py      
+rm run_local_console.log &&  ./demo_local55_logfile.py     
+
+echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
+
+cat run_local_console.log
