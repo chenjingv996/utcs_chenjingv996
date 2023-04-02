73debb4035b4230f4ff2fcc1de03ec852d96f2ba
chenjingv <chenjingv>
2023-04-02 22:11:58 +0800

    update_0820

--- jishi/run_autocheck55.sh
+++ jishi/run_autocheck55.sh
@@ -8,7 +8,7 @@ echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
 cat run_local_console_logfile.log
 
 else
-echo -e "\nwarning:the file is not exist,create it!\n"
+echo -e "\nwarning:the logfile is not exist,create it!\n"
 ./demo_local55_logfile.py	
 sleep 2
 echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
