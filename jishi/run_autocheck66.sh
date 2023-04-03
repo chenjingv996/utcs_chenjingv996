04464644196eb2c0ef50066f08fea7869749fb64
789 <789@qq.com>
2023-04-03 11:09:15 +0800

    update_0820

---
+++ jishi/run_autocheck66.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f "./run_office_console_logfile.log" ];then
+echo -e "\nwarning:the logfile is exist,delete it!\n"
+rm run_office_console_logfile.log &&  ./demo_office66_logfile.py     
+sleep 2
+echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
+cat run_office_console_logfile.log
+
+else
+echo -e "\nwarning:the logfile is not exist,create it!\n"
+./demo_office66_logfile.py	
+sleep 2
+echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
+cat run_office_console_logfile.log
+
+fi
