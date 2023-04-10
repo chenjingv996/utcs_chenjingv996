9350a037e2731c044e7b5ca242989b96a74a3334
789 <789@qq.com>
2023-04-10 12:54:19 +0800

    update_0820

--- jishi/run_autocheck66.sh
+++ jishi/run_autocheck66.sh
@@ -5,6 +5,7 @@ echo -e "\nwarning:the logfile is exist,delete it!\n"
 rm run_office_console_logfile.log &&  ./demo_office66_logfile.py     
 sleep 2
 echo -e "`seq -s '-' 80|sed s/[0-9]//g`\n"
+cp run_office_console_logfile.log /home/test/autotest
 cat run_office_console_logfile.log
 
 else
