e95bff82742373bd01558ff5b49f7a8fab9984cb
789 <789@qq.com>
2023-10-07 12:45:20 +0800

    update_0820

--- jishi/paixu_top3.py
+++ jishi/paixu_top3.py
@@ -12,28 +12,30 @@ print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
 
 
 class ccc:
-    def aaa(self,arr:list)->int:
-        if arr == None or len(arr) < 3:
+    def aaa(self,nums:list[int])->str:
+        if nums == None or len(nums) < 3:
             print("error!")
             return 
-        r1 = r2 = r3 = i = 0
-        while i < len(arr):
-            if arr[i] > r1:
+        r1 = r2 = r3 = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] > r1:
                 r3 = r2 
                 r2 = r1
-                r1 = arr[i]
-            elif arr[i] > r2 and arr[i] != r1:
+                r1 = nums[i]
+            elif nums[i] > r2 and nums[i] != r1:
                 r3 = r2
-                r2 = arr[i]
-            elif arr[i] > r3 and arr[i] != r2:
-                r3 =arr [i]
+                r2 = nums[i]
+            elif nums[i] > r3 and nums[i] != r2:
+                r3 = nums[i]
             i+=1
-        return r1,r2,r3
+        return str(f'{r1} {r2} {r3}')
 
 
 bbb=ccc()
 
+
 print(bbb.aaa([1,2]))
-print(bbb.aaa([3,1,4,2,5,3,6]))
+print(bbb.aaa([13,11,42,22,15,13,23,22,16]))
 
 
