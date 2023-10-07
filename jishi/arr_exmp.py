6aa3babb2c2ce6da2093abbd0b3a4b8b620f4476
789 <789@qq.com>
2023-10-07 15:34:36 +0800

    update_0820

---
+++ jishi/arr_exmp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+print("#"*80+"\n")
+
+import time
+from datetime import datetime
+import sys,re,math
+from array import array
+import numpy as np
+
+print(f"{time.ctime()}\n")
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}\n')
+
+
+arr1=array('i',[1,3,5,6])
+
+print(arr1[2])
+
+arr2=np.array([[3,5,2,4],[3,5,6,8]])
+
+print(arr2[1])
+
+arr3=np.array([13,22,34,55])
+
+print(arr3)
+
