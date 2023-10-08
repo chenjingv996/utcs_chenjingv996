366b26b886cf039acb8010a5d5b3d37a9af4cf65
789 <789@qq.com>
2023-10-08 12:14:17 +0800

    update_0820

---
+++ jishi/check_log.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+echo -e "\n*sing*.log\n"
+cat -n *sing*.log | grep ++ && cat -n *sing*.log | grep ++ | wc -l
+
+echo -e "\n*mult*.log\n"
+cat -n *mult*.log | grep ++ && cat -n *mult*.log | grep ++ | wc -l
+
