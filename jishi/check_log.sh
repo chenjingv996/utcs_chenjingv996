2f14b8c998bfdc04fcf61fc745a864b11c29ce5a
789 <789@qq.com>
2023-10-09 12:09:39 +0800

    update_0820

--- jishi/check_log.sh
+++ jishi/check_log.sh
@@ -1,8 +1,8 @@
 #/usr/bin/bash
 
 echo -e "\n*sing*.log\n"
-cat -n *sing*.log | grep ++ && cat -n *sing*.log | grep ++ | wc -l
+cat -n *sing*.log | egrep 'start|\++' && cat -n *sing*.log | grep ++ | wc -l
 
 echo -e "\n*mult*.log\n"
-cat -n *mult*.log | grep ++ && cat -n *mult*.log | grep ++ | wc -l
+cat -n *mult*.log | egrep 'start|\++' && cat -n *mult*.log | grep ++ | wc -l
 
