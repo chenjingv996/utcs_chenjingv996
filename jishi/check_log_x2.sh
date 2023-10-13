a0c4a4e093fba5e2ad2315cb4ee5f6a211dbd61a
789 <789@qq.com>
2023-10-13 19:01:05 +0800

    update_0820

---
+++ jishi/check_log_x2.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+echo -e "\n*x2_sing*.log\n"
+cat -n *x2_sing*.log | egrep 'time is|\++' && cat -n *x2_sing*.log | grep ++ | wc -l
+
+echo -e "\n*x2_mult*.log\n"
+cat -n *x2_mult*.log | egrep 'time is|\++' && cat -n *x2_mult*.log | grep ++ | wc -l
+
