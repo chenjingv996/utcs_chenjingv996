1872dfb20fc3a408bd3ff54c6651820ec5a94bbb
chenjingv <chenjingv>
2023-10-14 11:08:57 +0800

    update_0820

---
+++ jishi/demo_00.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import time as tm
+from datetime import datetime as dt
+
+print("\n"+"#"*80+"\n")
+
+print(f"\n当前时间为:{tm.ctime()}\n")
+print(f"\n当前时间为:{dt.now()}\n")
+
+
+class ccc:
+    def aaa(self,nums:list[int],target:int)->int:
+        l,r=0,len(nums)-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                return mid
+            elif target>nums[mid]:
+                l=mid+1
+            else:
+                r=mid-1
+        return -1
+
+bbb=ccc()
+print(bbb.aaa([1,2,3,4,5],2))
+
+
+print(f"\n")
