9f10fe36787ca161b604cfeec9aca6afa8d1681e
789 <789@qq.com>
2023-10-16 22:12:43 +0800

    update_0820

---
+++ jishi/piliang_script_x2.sh
@@ -0,0 +1,3 @@
+#/usr/bin/bash
+
+./autotest_x2_data_sing_vlan.py 3/3/1 1/1 && ./autotest_x2_data_mult_vlan.py 3/3/2 1/2
---
+++ jishi/piliang_script_x7.sh
@@ -0,0 +1,3 @@
+#/usr/bin/bash
+
+./autotest_x7_data_sing_vlan.py 3/3/1 10/1 && ./autotest_x7_data_mult_vlan.py 3/3/3 10/3
