644fffc69d40e58c8947e4637aa00bbdd90e15a1
chenjingv <chenjingv>
2023-10-21 00:19:26 +0800

    update_0820

---
+++ jishi/lc/lc_demo.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+
+class ccc:
+    def aaa(self,s:str)->int:
+        n=len(s)
+        ret,l,r=0,0,0
+        if n==0:
+            return 0
+        while r<n:
+            if s[r] not in s[l:r]:
+               r+=1
+               ret=max(ret,len(s[l:r]))
+               print(s[l:r]) #debug
+            else:
+                while s[r] in s[l:r]:
+                    l+=1
+        return ret
+
+bbb=ccc()
+print(bbb.aaa('cabcabcbb'))
+
