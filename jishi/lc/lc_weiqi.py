56246ef97fea2e3de86d84bfb65c0cc7c41e529d
chenjingv <chenjingv>
2023-10-23 23:25:17 +0800

    update_0820

---
+++ jishi/lc/lc_weiqi.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import numpy as np
+
+class ccc:
+    def aaa(self,nums:list[int])->str:
+        nd1=np.array(nums)
+        #print(nd1,type(nd1))
+        #return nd1,type(nd1)
+        return nd1
+
+
+bbb=ccc()
+print(bbb.aaa([1,2,3]))
+
