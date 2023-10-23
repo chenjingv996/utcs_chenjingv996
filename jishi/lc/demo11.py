771df6a4bd3e7d7d27a9d74ea0c212d7a09836c2
chenjingv <chenjingv>
2023-10-24 00:00:52 +0800

    update_0820

---
+++ jishi/lc/demo11.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import os,sys,time
+
+s=[int(i) for i in input().strip().split(',')]
+print(s)
+
+
--- jishi/lc/lc_king_box22.py
+++ jishi/lc/lc_king_box22.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 #coding:utf-8
 
-nums=[int(i) for i in input().strip().split(',')]
-#print(nums)   #debug
-k=int(input())
-l,r=0,k
-n=len(nums)
-res=sum(nums[0:k])
-ans=res
-while r<n:
-    ans+=nums[r]
-    ans-=nums[l]
-    r+=1
-    l+=1
-    res=max(res,ans)
-    print(ans)   #debug
-print(res)
+import os,sys,time
 
+class ccc:
+    def aaa(nums:list[int],k:int)->int:
+        for i in range(len(nums)):
+            s=','.join(nums[i]).strip()
+        return s
+
+
+bbb=ccc()
+print(bbb.aaa([1,2,-1,5,3],3))
